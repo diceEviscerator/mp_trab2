bab0108586f67f83f92a386fa929dce08668c53c
diceEviscerator <yurifleury12@gmail.com>
2017-04-17 21:09:50 -0300

    adicionado o código para passar no teste threeTest, corrigido um erro em que era usado || no lugar de && na clausula de string inválida

--- src/roman.cpp
+++ src/roman.cpp
@@ -8,13 +8,18 @@ int transf_roman (char *numb){
 		printf("Erro: string vazia\n"); return -1;
 	}
 	else{
-		for(i=0;i<=size; i++){
-			if((numb[i]!='I')||(numb[i]!='V')||(numb[i]!='X')||(numb[i]!='L')||(numb[i]!='C')||(numb[i]!='D')||(numb[i]!='M')){
+		for(i=0;i<size; i++){
+			if((numb[i]!='I')&&(numb[i]!='V')&&(numb[i]!='X')&&(numb[i]!='L')&&(numb[i]!='C')&&(numb[i]!='D')&&(numb[i]!='M')){
 				printf("Erro: string invalida\n");
 				return -1;
 			}
 		}
-
+		for(i=0;i<=size; i++){
+			if((numb[i]=='I')){
+				result++;
+			}
+		}
+		return result;
 	}
 	return 0;
 }
